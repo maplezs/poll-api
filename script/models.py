6eddc0d4b95005aa0b4a5fe5ddfe3829fce13118
ferrydarmawan.fd@gmail.com <ferrydarmawan.fd@gmail.com>
2023-06-01 20:06:45 +0700

    First push: create API for polling activity.

--- script/models.py
+++ script/models.py
@@ -1,12 +1,45 @@
 from script import db
 
-class Polling(db.Model): 
-    __tablename__ = 'polling_table'
+class User(db.Model):
+
+    __tablename__ = 'user_table'
 
     id = db.Column(db.String(16), primary_key=True)
-    name = db.Column(db.String(255), nullable=False, unique=True)
-    address = db.Column(db.String(255), nullable=False, unique=True)
-    link = db.Column(db.String(255), nullable=False, unique=True)
+    name = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    photo_url = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255), nullable=False)
+    createdAt = db.Column(db.Date)
+    updatedAt = db.Column(db.Date)
+
+    place = db.relationship('Place', backref='user', lazy=True)
 
     def __repr__(self):
-        return f"Polling('{self.id}', '{self.name}')"
+        return f"Place('{self.id, self.name}')"
+
+class Place(db.Model):
+    
+    __tablename__ = 'place_table'
+
+    id = db.Column(db.String(16), primary_key=True)
+    user_id = db.Column(db.String(16), db.ForeignKey('user_table.id'), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    address = db.Column(db.String(255), nullable=False)
+    createdAt = db.Column(db.Date)
+
+    poll = db.relationship('Poll', backref='place', lazy=True)
+ 
+    def __repr__(self):
+        return f"Place('{self.id, self.name}')"
+
+
+class Poll(db.Model): 
+    
+    __tablename__ = 'poll_table'
+
+    id = db.Column(db.Integer, primary_key=True)
+    place_id = db.Column(db.String(16), db.ForeignKey('place_table.id'), nullable=False)
+    createdAt = db.Column(db.Date)
+
+    def __repr__(self):
+        return f"Poll('{self.id}', '{self.createdAt}')"
--- script/routes.py
+++ script/routes.py
@@ -1,25 +1,42 @@
-from flask import request, render_template
+import nanoid
+from datetime import datetime
+from flask import request, jsonify, url_for, render_template
 
 from script import app, db
-from script.models import Polling
+from script.models import Place, Poll
 
-@app.route('/poll/<id>', methods=['POST'])
-def poll(id):
+@app.route('/poll/<string:id>', methods=['GET', 'POST'])
+def poll_submit(id):
+    place = Place.query.filter_by(id=id).first_or_404()
+    if request.method == 'GET':
+        place = Place.query.get_or_404(id)
+        poll_number = Poll.query.filter(Poll.place_id == id).count()
+        return render_template('poll.html', place=place, poll_number=poll_number)
+    else:
+        poll = Poll(place_id=id, createdAt=datetime.now())
+        db.session.add(poll)
+        db.session.commit()
+        place = Place.query.get_or_404(id)
+        poll_number = Poll.query.filter(Poll.place_id == id).count()
+        return jsonify({
+            'status': 'sukses',
+            'message': 'Voting berhasil dilakukan!',
+            'data': poll_number
+        }) 
 
-    content = request.json
-    
-    name = content['name']
-    link = content['link']
-    address = content['address']
-
-    polling = Polling(
-        id=id,
-        name=name,
-        link=link,
-        address=address
-    )
-
-    db.session.add(polling)
+@app.route('/poll/create', methods=['POST'])
+def poll_create():
+    id = nanoid.generate(size=16)
+    data = request.json
+    place = Place(user_id=data['user_id'], id=id, name=data['name'], address=data['address'], createdAt=datetime.now())
+    db.session.add(place)
     db.session.commit()
+    return jsonify({
+        'status': 'sukses',
+        'message': 'Form poll berhasil dibuat!',
+        'data': f'localhost:5000/poll/{id}'
+    })
 
-    return render_template('home.html', name=name, link=link, address=address, id=id)
+@app.route('/test')
+def test():
+    return render_template('hello.html')
